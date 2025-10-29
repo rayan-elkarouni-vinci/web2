9b23f1b450d07f6b4642cb18508fa9996ac694b3
Rayan PC <elkarounirayan@outlook.com>
2025-10-29 18:37:54 +0100

    new:ex2.3

---
+++ exercices/2.1-2-3-4/src/components/Cinema/index.tsx
@@ -0,0 +1,29 @@
+import "./Cinema.css";
+
+interface CinemaProps {
+    name: string;
+    movies: Movie[]; // un tableau de films
+}
+
+interface Movie {
+    title: string;
+    director: string;
+}
+
+
+const Cinema = (props: CinemaProps) => {
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <ul>
+          {props.movies.map((movie) => (
+            <li key ={movie.title}>
+              <strong>{movie.title}</strong> - Réalisateur : {movie.director}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+};
+
+export default Cinema;
---
+++ exercices/2.1-2-3-4/src/components/PageTitle/index.tsx
@@ -0,0 +1,12 @@
+import "./PageTitle.css";
+
+interface PageTitleProps {
+    title: string;
+}
+
+
+const PageTitle = (props: PageTitleProps) => {
+    return <h1>{props.title}</h1>;
+};
+
+export default PageTitle;
