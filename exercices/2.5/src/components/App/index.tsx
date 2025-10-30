1a5565fb43c91f3c7c7473807be21108a34294a9
Rayan PC <elkarounirayan@outlook.com>
2025-10-30 23:43:01 +0100

    new:ex2.5-events

--- exercices/2.5/src/components/App/index.tsx
+++ exercices/2.5/src/components/App/index.tsx
@@ -16,7 +16,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <ClickCounter title="Titre Destructuré: Compteur de Clics" message="You are a master in the art of clicking !"/>
+        <ClickCounter title="Compteur de Clics (destructuré)" message="You are a master in the art of clicking !" hoverMessage="Please click on me now !"/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
